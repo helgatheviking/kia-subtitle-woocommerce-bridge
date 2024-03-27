93a265c607731128c0d79e7de81a7a8b03ba53a9
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-03-27 08:54:28 -0400

    update tested version and copyright

--- kia-subtitle-woocommerce-bridge.php
+++ kia-subtitle-woocommerce-bridge.php
@@ -8,10 +8,10 @@
  * Author: Kathy Darling
  * Author URI: http://kathyisawesome.com
  * Requires at least: 4.5.0
- * Tested up to: 5.9.3
+ * Tested up to: 6.5.0
  * WC requires at least: 3.5.0    
  *
- * Copyright: © 2018 Kathy Darling.
+ * Copyright: © 2024 Kathy Darling.
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
